d19fed4a835864786047a83109206020e29b78e2
George Harris <george.s.harris@gmail.com>
2026-01-17 17:52:25 +0000

    added some error handling in pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -20,20 +20,30 @@ def gather_game_id_names_ranks_from_html_pages() -> list[GameRankCreate]:
     """
     html_pages = HTMLPages()
     page_1 = html_pages.fetch_ranking_page(page=1)
-    max_page_number = 3#get_html_last_page_number(page_1)
 
-    collected_pages = html_pages.fetch_ranking_pages(start=2, stop=max_page_number)
-    collected_pages.insert(0, page_1)
-
-    collected_game_ids_names_ranks = []
-
-    for page in collected_pages:
-        collected_game_ids_names_ranks.append(parse_html_ranking_page(page))
+    if page_1 == None:
+        logger.error("PAGE 1 HAS NOT BEEN FETCHED CORRECTLY!")
+        raise ValueError("Page 1 has not been fetched correctly!")
     
-    # bring these together
-    collected_game_ids_names_ranks = list(chain.from_iterable(collected_game_ids_names_ranks))
+    else:
+        max_page_number = get_html_last_page_number(page_1)
 
-    return collected_game_ids_names_ranks
+        if max_page_number != None:
+            collected_pages = html_pages.fetch_ranking_pages(start=2, stop=max_page_number)
+            collected_pages.insert(0, page_1)
+
+            collected_game_ids_names_ranks = []
+
+            for page in collected_pages:
+                collected_game_ids_names_ranks.append(parse_html_ranking_page(page))
+            
+            # bring these together
+            collected_game_ids_names_ranks = list(chain.from_iterable(collected_game_ids_names_ranks))
+
+            return collected_game_ids_names_ranks
+        else:
+            logger.error("COUILD NOT FIND A MAX PAGE NUMBER FROM PAGE 1!")
+            raise ValueError("Could not find a max page number from page 1!")
 
 
 def main_pipeline() -> None:
@@ -59,10 +69,12 @@ def main_pipeline() -> None:
         db.bulk_insert_mappings(Game, games) # type: ignore
         logger.info("COMMITING TO DB")
         db.commit()
+
     except Exception as e:
         db.rollback()
         logger.error(f"BULK INSERT OF GAME IDS, NAMES AND RANKS FAILED WITH FOLLOWING ERROR: \n{e}")
         raise e
+    
     finally:
         db.close()
 
